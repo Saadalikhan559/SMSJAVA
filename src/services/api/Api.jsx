84a535c338a08424dadcc5240f613e88db6c7d1c
Arsalan Ahmed <arsalanahmed7999@gmail.com>
2025-08-20 15:13:41 +0530

    admission edit form

--- src/services/api/Api.jsx
+++ src/services/api/Api.jsx
@@ -705,10 +705,6 @@ export const handleEditAdmissionForm = async (formData, id) => {
         },
       }
     );
-    if (response.status === 200 || response.status === 201) {
-      alert("successfully submitted the form");
-    }
-
     return response.data;
   } catch (err) {
     throw err;
