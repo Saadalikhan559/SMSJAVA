4b6fa7b80f111cbdea0bf3aebc028fd8208625d3
Arsalan Ahmed <arsalanahmed7999@gmail.com>
2025-08-26 10:32:01 +0530

    new commit

--- src/global/constants.jsx
+++ src/global/constants.jsx
@@ -3,7 +3,7 @@ export const constants = {
   isOffline: true,
   // baseUrl: "https://gl8tx74f-8000.inc1.devtunnels.ms",
   // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen
-  baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms", //farha
+  // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms", //farha
   // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen
   // baseUrl: "https://2gqlk571-8000.inc1.devtunnels.ms", //saqib
   // baseUrl: "https://187gwsw1-7000.inc1.devtunnels.ms", //farha
@@ -15,7 +15,7 @@ export const constants = {
   // baseUrl: "https://2gqlk571-8000.inc1.devtunnels.ms", //saqib
   // baseUrl: "https://94f38xkg-8000.inc1.devtunnels.ms/", //naaz
 
-//   baseUrl: "https://94f38xkg-8000.inc1.devtunnels.ms/", //naaz
+  baseUrl: "https://94f38xkg-7000.inc1.devtunnels.ms/", //naaz
 
 
   hideEdgeRevealStyle: `
