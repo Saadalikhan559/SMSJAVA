e1066548dfbd8f3579248382b2e0cd14748ee9bd
maqbul ansari <maqboolansari@Maqbools-Mac-mini.local>
2025-08-28 18:21:12 +0530

    worked

---
+++ src/components/ErrorFallBack.jsx
@@ -0,0 +1,21 @@
+
+
+const ErrorFallBack = () => {
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+       <h1 className="text-6xl font-bold">500</h1>
+      <h1 className='text-2xl'>Something went Wrong</h1>
+      <p className="py-6">
+       We're sorry,  It looks like something broke on our end. We're working to fix it as soon as possible. Please go back to the homepage! 
+      </p>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default ErrorFallBack
