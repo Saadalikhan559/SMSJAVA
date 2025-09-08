f5c3b1c369e9184da06a23d6fb757fae2aa6d071
Abdurrahmankhan11 <abdurrahmankhan1429@gmail.com>
2025-09-08 17:33:05 +0530

    [Notification]

--- src/global/constants.jsx
+++ src/global/constants.jsx
@@ -1,7 +1,7 @@
 export const constants = {
   isOnline: true,
   isOffline: true,
-  // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms", //farha
+  baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms", //farha
   // baseUrl: "https://187gwsw1-7000.inc1.devtunnels.ms", //farha
   // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
   // baseUrl: "https://2gqlk571-8000.inc1.devtunnels.ms", //saqib
