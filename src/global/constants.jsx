1478e774f477973ab4174cb725e77178de7f7f55
sms-admin-technohub <167866423+sms-admin-technohub@users.noreply.github.com>
2025-09-26 15:12:58 +0530

    Update constants.jsx

--- src/global/constants.jsx
+++ src/global/constants.jsx
@@ -3,10 +3,9 @@ export const constants = {
   isOffline: true,
 
 
-
   // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms/", //farha     
   // baseUrl: "https://9gqxjbjg-8000.inc1.devtunnels.ms/", //tahur     
-  baseUrl: "https://smsproject1.pythonanywhere.com/", //main     
+  baseUrl: "https://smsproject1.pythonanywhere.com/", // main branch    
   // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms/", //farha     
   //  baseUrl: "https://9gqxjbjg-8000.inc1.devtunnels.ms/", //tahur      
   // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
