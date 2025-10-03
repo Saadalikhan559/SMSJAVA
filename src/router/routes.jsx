9d3624ae83d7d8676696b4d6b4e2d35d14d590be
maqbul ansari <maqbul.ansari.dev@gmail.com>
2025-10-03 19:18:49 +0530

    worked

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -84,6 +84,7 @@ import { UpdateSalaryExpense } from "../components/ManageExpenses/SalaryExpense/
 import { EmployeeMonthlySalary } from "../components/ManageExpenses/SalaryExpense/EmployeeMonthlySalary";
 import PrivacyPolicy from "../components/Privacy/Privacy";
 import CreateCategory from "../components/ManageExpenses/AllExpenses/CreateCategory";
+import { StudentAdmissionFees } from "../components/AdmissionProcess/StudentAdmissionFees";
 
 
 export const routes = [
