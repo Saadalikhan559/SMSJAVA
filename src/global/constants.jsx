6249f34d65c14224eb87a713da9d7be438c772ed
maqbul ansari <maqbul.ansari.dev@gmail.com>
2025-10-04 21:47:21 +0530

    worked

--- src/global/constants.jsx
+++ src/global/constants.jsx
@@ -4,9 +4,9 @@ export const constants = {
 
   // baseUrl: "https://smsproject1.pythonanywhere.com/", //deployed for testing    
 
-  // baseUrl: "https://smsproject.pythonanywhere.com/", //deployed for school     
+  baseUrl: "https://smsproject.pythonanywhere.com/", //deployed for school     
 
-  baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms/", //farha     
+  // baseUrl: "https://187gwsw1-8000.inc1.devtunnels.ms/", //farha     
   //  baseUrl: "https://9gqxjbjg-8000.inc1.devtunnels.ms/", //tahur      
 
   // baseUrl: "https://gl8tx74f-7000.inc1.devtunnels.ms", //farheen                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
