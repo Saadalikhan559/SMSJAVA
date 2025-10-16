ab9de16478b075fa2ea7c26b753a314f2174331e
maqbul ansari <maqbul.ansari.dev@gmail.com>
2025-10-16 17:11:13 +0530

    worked

--- src/components/StaffData/UpdateStaffdetails.jsx
+++ src/components/StaffData/UpdateStaffdetails.jsx
@@ -223,7 +223,7 @@ const handleSubmit = async (e) => {
   }
 
   return (
-    <>
+    <div className="mb-24 md:mb-10">
       <div className="p-6 bg-gray-100 dark:bg-gray-900 min-h-screen">
         <div className="max-w-7xl mx-auto bg-white dark:bg-gray-800 p-8 rounded-lg shadow dark:shadow-gray-700">
           <h1 className="text-3xl font-bold mb-8 text-center text-gray-800 dark:text-gray-100">
@@ -343,7 +343,7 @@ const handleSubmit = async (e) => {
           handleCloseAndNavigate={() => navigate(`/staffDetail/${type}/${id}`)}
         />
       )}
-    </>
+    </div>
   );
 };
 
